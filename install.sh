bc858be14b90bbff23332fd2e033d95d94194df9
Diego Carvalho <d.carvalho@ieee.org>
2020-12-14 15:02:14 -0300

    slipt import

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 JOBDIR=/scratch/cenapadrjsd/diego.carvalho/biocomp
  
 mkdir -p $JOBDIR/tmp
-cp -R Astral parsl.env work.config parsl_inside_allocation.py raxml-phase1.pl submit.sh $JOBDIR
+cp -R Astral parsl.env work.config parsl_inside_allocation.py raxml-phase1.pl workflow.py submit.sh $JOBDIR
