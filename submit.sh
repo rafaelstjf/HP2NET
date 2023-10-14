76ff0f3048e19f44e41d084c53817d901aee6039
rafael <rafaelstjf@hotmail.com>
2023-10-14 14:03:52 -0300

    merge on sdumont

--- submit.sh
+++ submit.sh
@@ -4,7 +4,7 @@
 #SBATCH -p cpu_small         #Fila (partition) a ser utilizada
 #SBATCH -J Biocomp             #Nome job
 #SBATCH --exclusive          #Utilização exclusiva dos nós durante a execução do job
-#SBATCH --time=15:00:00
+#SBATCH --time=08:00:00
 #SBATCH -e slurm-%j.err
 #SBATCH -o slurm-%j.out
 
@@ -12,9 +12,14 @@
 
 
 echo Loading modules
-module load python/3.8.2
+module load python/3.9.6
+source $SCRATCH/meu_python/bin/activate
 #acessa o diretório onde o script está localizado
 cd /scratch/pcmrnbio2/rafael.terra/WF_parsl/biocomp_single
 #executa o script
 echo Starting Workflow Script
-python3 parsl_workflow.py --wf $WF --cf 1_thread.ini
+echo Workload file: $WF
+echo Maximum workers: $MW
+python3 parsl_workflow.py --wf $WF --cf 1_thread.ini --mw $MW
+
+#sbatch --export=ALL,SCRATCH=/scratch/pcmrnbio2/rafael.terra,WF=100g6t_iqtree_snaq.config,MW=1 submit.sh 
