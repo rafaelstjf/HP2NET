f77d40dc0b8a17c5c4e90460e391b8e72d6a0bb9
Rafael Terra <rafaelstjf@hotmail.com>
2024-06-17 17:23:07 -0300

    removed some unuse code

--- utils.py
+++ utils.py
@@ -25,56 +25,6 @@ class Cache():
         else:
             return self._table[key]
 
-class _HashedTuple(tuple):
-    """A tuple that ensures that hash() will be called no more than once
-    per element, since cache decorators will hash the key multiple
-    times on a cache miss.  See also _HashedSeq in the standard
-    library functools implementation.
-
-    """
-
-    __hashvalue = None
-
-    def __hash__(self, hash=tuple.__hash__):
-        hashvalue = self.__hashvalue
-        if hashvalue is None:
-            self.__hashvalue = hashvalue = hash(self)
-        return hashvalue
-
-    def __add__(self, other, add=tuple.__add__):
-        return _HashedTuple(add(self, other))
-
-    def __radd__(self, other, add=tuple.__add__):
-        return _HashedTuple(add(other, self))
-
-    def __getstate__(self):
-        return {}
-    def getValue(self):
-        return self.__hashvalue
-
-
-# used for separating keyword arguments; we do not use an object
-# instance here so identity is preserved when pickling/unpickling
-_kwmark = (_HashedTuple,)
-
-
-def hashkey(*args, **kwargs):
-    """Return a cache key for the specified hashable arguments."""
-
-    kwargs_filtered = dict()
-    for k in kwargs.keys():
-        if type(kwargs[k]) != dict:
-            kwargs_filtered[k] = kwargs[k]
-    args_filtered = tuple()
-    for arg in args:
-        if type(arg) != dict:
-            args_filtered.append(arg)
-    if kwargs:
-        return _HashedTuple(args_filtered + sum(sorted(kwargs_filtered.items()), _kwmark))
-    else:
-        return _HashedTuple(args_filtered)
-
-
 def tuplify(arguments: dict):
     return tuple((k, tuple(v)) for k, v in arguments.items())
 
