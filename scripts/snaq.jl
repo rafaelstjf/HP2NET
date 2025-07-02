073c9ac80e2287f7760f392292c848ca98d22c2d
Rafael Terra <rafaelstjf@gmail.com>
2025-07-02 13:12:32 -0300

    seed to snaq

--- scripts/snaq.jl
+++ scripts/snaq.jl
@@ -8,17 +8,20 @@
 # ARGS[5] = num_workers
 # ARGS[6] = hmax
 # ARGS[7] = runs
-# ARGS[8] = (optional) outgroup
+# ARGS[8] = seed
+# ARGS[9] = (optional) outgroup
+
+#!/bin/env julia
+
+using Random
 
 println("Starting PhyloNetworks...")
 
-# Validate arguments
-if length(ARGS) < 7
-    println("Usage: script.jl <tree method> <tree path> <topology path> <output dir> <num_workers> <hmax> <runs> [outgroup]")
+if length(ARGS) < 8 || length(ARGS) > 9
+    println("Usage: script.jl <tree method> <tree path> <topology path> <output dir> <num_workers> <hmax> <runs> <seed> [outgroup]")
     exit(1)
 end
 
-# Parse required arguments
 method = ARGS[1]
 tree_path = ARGS[2]
 topology_path = ARGS[3]
@@ -26,7 +29,15 @@ output_dir = ARGS[4]
 num_workers = max(1, parse(Int, ARGS[5]) - 1)
 hmax = parse(Int, ARGS[6])
 runs = parse(Int, ARGS[7])
-outgroup = get(ARGS, 8, nothing)  # Optional
+seed = parse(Int, ARGS[8])
+
+if length(ARGS) == 8
+    # Sem outgroup, seed é ARGS[8]
+    outgroup = nothing
+elseif length(ARGS) == 9
+    # Com outgroup, seed é ARGS[9]
+    outgroup = ARGS[9]
+end
 
 println("Tree method: $method")
 println("Tree path: $tree_path")
@@ -38,6 +49,8 @@ println("Number of runs: $runs")
 if outgroup !== nothing
     println("Species mapping: $outgroup")
 end
+println("Using random seed: $seed")
+Random.seed!(seed)
 import Pkg
 
 # Ensure required packages are installed
@@ -92,12 +105,12 @@ if method in ["RAXML", "IQTREE"]
         raxmlCF = readTrees2CF(tree_path, writeTab=false, writeSummary=false)
     end
     astraltree = readTopology(last(readlines(topology_path)))
-    net = snaq!(astraltree, raxmlCF, hmax=hmax, filename=output, runs=runs)
+    net = snaq!(astraltree, raxmlCF, hmax=hmax, filename=output, runs=runs, seed=seed)
 
 elseif method == "MRBAYES"
     buckyCF = readTableCF(tree_path)
     qmc_tree = readTopology(topology_path)
-    net = snaq!(qmc_tree, buckyCF, hmax=hmax, filename=output, runs=runs)
+    net = snaq!(qmc_tree, buckyCF, hmax=hmax, filename=output, runs=runs, seed=seed)
 
 else
     println("Invalid tree method! Supported methods: RAXML, IQTREE, MRBAYES")
